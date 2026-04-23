5cb302b26dbfc6eaa4b4b9d1299d63df4b226c9f
Naman-31-nsk <codebreakernaman@gmail.com>
2026-04-23 23:54:26 +0530

    Create app_sizes.dart

---
+++ lib/core/utils/app_sizes.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xff6C63FF);
+  static const secondary = Color(0xff8E2DE2);
+  static const white = Colors.white;
+  static const black = Colors.black;
+}
